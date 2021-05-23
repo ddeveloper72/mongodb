03cd48243016ae97def87ce76bab297db7027a0c
ddeveloper72 <duncan.niamh@gmail.com>
2021-05-23 17:15:40 +0100

    updated to use .env URI

--- 02_mongo_insert.py
+++ 02_mongo_insert.py
@@ -1,9 +1,14 @@
-import pymongo
 import os
+import pymongo
+from dotenv import load_dotenv
+
+load_dotenv()
 
 MONGODB_URI = os.getenv("MONGO_URI")
+
 DBS_NAME = "mytestdb"
 COLLECTION_NAME = "myFirstMDB"
+
 
 def mongo_connect(url):
     try:
@@ -12,19 +17,25 @@ def mongo_connect(url):
         return conn
     except pymongo.errors.ConnectionFailure as e:
         print("Could not connect to MongoDB: %s") % e
-        
+
 conn = mongo_connect(MONGODB_URI)
 
 coll = conn[DBS_NAME][COLLECTION_NAME]
 
-new_doc = {'first': 'saoirse', 'last': 'ronan', 'dob': '12/04/1994', 
-    'hair_colour': 'blond', 'gender': 'f', 'occupation': 'actress', 'nationality': 'irish'}
+# New document to be added
+new_doc = {
+    'first': 'saoirse',
+    'last': 'ronan',
+    'dob': '12/04/1994',
+    'hair_colour': 'blond',
+    'gender': 'f',
+    'occupation': 'actress',
+    'nationality': 'irish'
+}
 
-coll.insert(new_doc)
+coll.insert_one(new_doc)
 
 documents = coll.find()
 
 for doc in documents:
     print(doc)
-    
-
