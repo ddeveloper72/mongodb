fa34397d70cc64c8b4cce73cd01761d40114fba0
ddeveloper72 <duncan.niamh@gmail.com>
2021-05-24 14:22:10 +0100

    added .env URI for mongoDB

--- mongo_project_pt5.py
+++ mongo_project_pt5.py
@@ -1,9 +1,13 @@
-import pymongo
 import os
+import pymongo
+from dotenv import load_dotenv
+
+load_dotenv()
 
 MONGODB_URI = os.getenv("MONGO_URI")
 DBS_NAME = "mytestdb"
 COLLECTION_NAME = "myFirstMDB"
+
 
 def mongo_connect(url):
     try:
@@ -11,34 +15,37 @@ def mongo_connect(url):
         return conn
     except pymongo.errors.ConnectionFailure as e:
         print("Could not connect to MongoDB: %s") % e
-        
-def show_menu(): # CRUD Create, Read, Update, Delete
+
+
+def show_menu():  # CRUD Create, Read, Update, Delete
     print("")
     print("1. Add a record")
     print("2. Find a record by name")
     print("3. Edit a record")
     print("4. Delete a record")
     print("5. Exit")
-    
-    option = input ("Enter option: ")
+
+    option = input("Enter option: ")
     return option
+
 
 def get_record():
     print("")
     first = input("Enter first name > ")
     last = input("Enter last name > ")
-    
+
     try:
         doc = coll.find_one({'first': first.lower(), 'last': last.lower()})
     except:
         print("Error accessing the database")
-        
+
     if not doc:
         print("")
         print("Error! No record found.")
-    
+
     return doc
-   
+
+
 def add_record():
     print("")
     first = input("Enter first name > ")
@@ -48,35 +55,43 @@ def add_record():
     hair_colour = input("Enter hair colour > ")
     occupation = input("Enter occupation > ")
     nationality = input("Enter nationality > ")
-    
-    new_doc = {'first': first.lower(), 'last': last.lower(), 'dob': dob, 
-        'gender': gender, 'hair_colour': hair_colour, 'occupation': occupation, 
-        'nationality': nationality}
-    
+
+    new_doc = {
+        'first': first.lower(),
+        'last': last.lower(),
+        'dob': dob,
+        'gender': gender.lower(),
+        'hair_colour': hair_colour.lower(),
+        'occupation': occupation.lower(),
+        'nationality': nationality.lower()
+    }
+
     try:
         coll.insert(new_doc)
         print("")
         print("Document inserted")
     except:
         print("Error accessing the datbase")
-        
+
+
 def find_record():
     doc = get_record()
     if doc:
         print("")
-        for k,v in doc.items():
+        for k, v in doc.items():
             if k != "_id":
                 print(k.capitalize() + ": " + v.capitalize())
+
 
 def edit_record():
     doc = get_record()
     if doc:
-        update_doc={}
+        update_doc = {}
         print("")
-        for k,v in doc.items():
+        for k, v in doc.items():
             if k != "_id":
                 update_doc[k] = input(k.capitalize() + " [" + v + "] > ")
-                
+
                 if update_doc[k] == "":
                     update_doc[k] = v
 
@@ -85,7 +100,8 @@ def edit_record():
             print("")
         except:
             print("Error accessing the datbase")
-            
+
+
 def main_loop():
     while True:
         option = show_menu()
@@ -104,15 +120,9 @@ def main_loop():
             print("Invalid option")
         print("")
 
+
 conn = mongo_connect(MONGODB_URI)
 
 coll = conn[DBS_NAME][COLLECTION_NAME]
 
 main_loop()
-
-        
-        
-    
-
-    
-    
